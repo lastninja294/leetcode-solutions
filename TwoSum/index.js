0a068476da99ac6209ee18edf3415c9d61cd30bd
lastninja294 <lastninja294@gmail.com>
2023-03-13 13:56:05 +0500

    initial commit

---
+++ TwoSum/index.js
@@ -0,0 +1,19 @@
+// Question => https://leetcode.com/problems/two-sum/
+
+// Solution
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const mp = {}
+
+   for (let i = 0; i < nums.length; i++) {
+   	const diff = target - nums[i]
+   
+   	if (diff in mp) return [i, mp[diff]]
+
+   	mp[nums[i]] = i
+   }
+};
